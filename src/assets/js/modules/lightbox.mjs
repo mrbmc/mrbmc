e70deff4afb84a031ae9ecbdcf0add09d690e7d1
Brian McConnell <brian@mrbmc.com>
2025-09-27 17:25:36 -0400

    added prev & next to lightbox

--- src/assets/js/modules/lightbox.mjs
+++ src/assets/js/modules/lightbox.mjs
@@ -1,25 +1,55 @@
 import { parseSrcset } from "./dom_utils.mjs";
 
 let bHasLightboxes = false;
+window.lightboxIndex = [];
 
+function prevImage() {
+    console.log('lightbox.previmage');
+    let cur = document.querySelector('#lightbox img').src;
+    window.lightboxIndex.forEach((obj,index)=>{
+        let src = getSrc(obj);
+        if(cur.indexOf(src)>=0) {
+            console.log("match",index);
+            let newSrc = getSrc(window.lightboxIndex[(index-1)]);
+            document.querySelector('#lightbox img').src = newSrc;
+            return;
+        }
+    })
+}
+function nextImage() {
+    console.log('lightbox.nextimage');
+    let cur = document.querySelector('#lightbox img').src;
+    window.lightboxIndex.forEach((obj,index)=>{
+        let src = getSrc(obj);
+        if(cur.indexOf(src)>=0) {
+            console.log("match",index);
+            let newSrc = getSrc(window.lightboxIndex[(index+1)]);
+            document.querySelector('#lightbox img').src = newSrc;
+            return;
+        }
+    })
+}
+
+function getSrc(element) {
+    var newSrc = element.src,
+        srcSet = parseSrcset(element.srcset).pop().url;
+
+    if(srcSet!=""){
+        newSrc = srcSet;
+    }
+
+    return newSrc;
+}
 
 export function addLightbox (obj) {
-    console.log('addLightbox',obj);
+    // console.log('addLightbox',obj);
 
     if(!bHasLightboxes) initLightbox();
 
+    window.lightboxIndex.push(obj);
+
     obj.addEventListener('click', function(e) {
-        var newObj = document.createElement('img'),
-            newSrc = this.src,
-            srcSet = parseSrcset(this.srcset).pop().url;
-
-        if(srcSet!=""){
-            newSrc = srcSet;
-        }
-
-        newObj.setAttribute('src', newSrc);
-        document.getElementById('lightbox').innerHTML = "";
-        document.getElementById('lightbox').append(newObj);
+        document.querySelector('#lightbox img').src = getSrc(this);
         document.getElementById('lightbox').classList.add('in');
     })
 
@@ -34,6 +64,8 @@ function initLightbox () {
 
     let el = document.createElement('div');
         el.id = "lightbox";
+    let img = document.createElement('img');
+        el.append(img);
     document.getElementsByTagName('main')[0].append(el);
 
     el.addEventListener('click',()=>{
@@ -46,6 +78,12 @@ function initLightbox () {
         switch(e.keyCode) {
             case 27://escape
               document.getElementById('lightbox').classList.remove("in");
+              break;
+            case 37://left arrow
+                prevImage();
+              break;
+            case 39://right arrow
+                nextImage();
               break;
             default:
         }
